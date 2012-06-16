587dbe1c825955985613e0a1e7cbe540fb817a3e
Patrick Richards <p5richards@mac.local>
2012-06-16 11:52:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstrails::Application.config.secret_token = '16ee88a5c6e4ce10343fd007eb9b6a268ad205c3912149747c802c537d69406b0ffd7080cb0249d1b21e96555b1c41e5184a285e970f8c2478e85380fc959065'
