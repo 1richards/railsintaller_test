d450a552bb037475d80da20735846f8e1a0ca9c0
Patrick Richards <p5richards@mac.local>
2012-06-17 13:51:43 -0400

    day 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Firstrails::Application.routes.draw do
+  resources :posts do
+	resources :comments
+	end
+
+  get "home/index"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +56,7 @@ Firstrails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
